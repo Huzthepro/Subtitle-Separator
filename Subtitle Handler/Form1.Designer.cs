f5575007dcc9df3897b0bbe679ef4dbfdaa9d6ca
Huzeyfe <114608730+Huzthepro@users.noreply.github.com>
2023-12-18 20:14:45 +0100

    Deleted: Close and minimize btn

--- Subtitle Handler/Form1.Designer.cs
+++ Subtitle Handler/Form1.Designer.cs
@@ -28,46 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            closeAppBtn = new Button();
-            minAppBtn = new Button();
             SuspendLayout();
-            // 
-            // closeAppBtn
-            // 
-            closeAppBtn.Location = new Point(762, 12);
-            closeAppBtn.Name = "closeAppBtn";
-            closeAppBtn.Size = new Size(26, 25);
-            closeAppBtn.TabIndex = 0;
-            closeAppBtn.Text = "X";
-            closeAppBtn.UseVisualStyleBackColor = true;
-            closeAppBtn.Click += closeAppBtn_Click;
-            // 
-            // minAppBtn
-            // 
-            minAppBtn.Location = new Point(730, 12);
-            minAppBtn.Name = "minAppBtn";
-            minAppBtn.Size = new Size(26, 25);
-            minAppBtn.TabIndex = 1;
-            minAppBtn.Text = "-";
-            minAppBtn.UseVisualStyleBackColor = true;
-            minAppBtn.Click += minAppBtn_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(minAppBtn);
-            Controls.Add(closeAppBtn);
-            FormBorderStyle = FormBorderStyle.None;
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
         }
 
         #endregion
-
-        private Button closeAppBtn;
-        private Button minAppBtn;
     }
 }
