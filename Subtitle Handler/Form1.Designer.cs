79add0ae68977a1eaf3f734200d62c0a12ef8e70
Huzeyfe <114608730+Huzthepro@users.noreply.github.com>
2023-12-20 22:25:03 +0100

    Added: Save Btn

--- Subtitle Handler/Form1.Designer.cs
+++ Subtitle Handler/Form1.Designer.cs
@@ -30,11 +30,15 @@
         {
             dataGridView = new DataGridView();
             panelMenu = new Panel();
+            button2 = new Button();
+            button1 = new Button();
             ((System.ComponentModel.ISupportInitialize)dataGridView).BeginInit();
+            panelMenu.SuspendLayout();
             SuspendLayout();
             // 
             // dataGridView
             // 
+            dataGridView.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             dataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dataGridView.Location = new Point(12, 45);
             dataGridView.Name = "dataGridView";
@@ -43,12 +47,44 @@
             // 
             // panelMenu
             // 
-            panelMenu.BackColor = Color.FromArgb(51, 51, 76);
+            panelMenu.BackColor = Color.Transparent;
+            panelMenu.Controls.Add(button2);
+            panelMenu.Controls.Add(button1);
             panelMenu.Dock = DockStyle.Top;
             panelMenu.Location = new Point(0, 0);
             panelMenu.Name = "panelMenu";
             panelMenu.Size = new Size(799, 39);
             panelMenu.TabIndex = 1;
+            // 
+            // button2
+            // 
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.ForeColor = SystemColors.ButtonFace;
+            button2.Image = Properties.Resources.save32;
+            button2.Location = new Point(63, 0);
+            button2.Margin = new Padding(0);
+            button2.Name = "button2";
+            button2.Size = new Size(32, 32);
+            button2.TabIndex = 3;
+            button2.TextAlign = ContentAlignment.MiddleLeft;
+            button2.TextImageRelation = TextImageRelation.ImageBeforeText;
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.ForeColor = SystemColors.ButtonFace;
+            button1.Image = Properties.Resources.open_folder32;
+            button1.Location = new Point(12, 0);
+            button1.Margin = new Padding(0);
+            button1.Name = "button1";
+            button1.Size = new Size(32, 32);
+            button1.TabIndex = 2;
+            button1.TextAlign = ContentAlignment.MiddleLeft;
+            button1.TextImageRelation = TextImageRelation.ImageBeforeText;
+            button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -60,6 +96,7 @@
             Name = "Form1";
             Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)dataGridView).EndInit();
+            panelMenu.ResumeLayout(false);
             ResumeLayout(false);
         }
 
@@ -67,5 +104,7 @@
 
         private DataGridView dataGridView;
         private Panel panelMenu;
+        private Button button1;
+        private Button button2;
     }
 }
