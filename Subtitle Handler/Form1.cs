6d4a93575a5e7f0fccc4e0ec4b70364531d993ca
Huzeyfe <114608730+Huzthepro@users.noreply.github.com>
2023-12-20 23:57:34 +0100

    Removing control box

--- Subtitle Handler/Form1.cs
+++ Subtitle Handler/Form1.cs
@@ -1,3 +1,5 @@
+using System.Runtime.InteropServices;
+
 namespace Subtitle_Handler
 {
     public partial class Form1 : Form
@@ -5,7 +7,16 @@ namespace Subtitle_Handler
         public Form1()
         {
             InitializeComponent();
+            //Removing name and the control box
+            this.Text = string.Empty;
+            this.ControlBox = false;    
+
         }
+        //Since control box is gone theese are for dragging the window
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
 
         private void label1_Click(object sender, EventArgs e)
         {
